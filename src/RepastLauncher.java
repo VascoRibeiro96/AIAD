d0580eb898b1175951a44a6b7283ac1bef85a825
zecst19 <zecst19@gmail.com>
2017-12-05 16:00:46 +0000

    repasta-me os 🍅's

---
+++ src/RepastLauncher.java
@@ -0,0 +1,27 @@
+import uchicago.src.sim.engine.SimModelImpl;
+
+public class RepastLauncher {
+	
+	  public String getName(){
+	    return "Parques de Estacionamento";
+	  }
+
+	  public void setup(){
+	  }
+
+	  public void begin(){
+	    buildModel();
+	    buildSchedule();
+	    buildDisplay();
+	  }
+
+	  public void buildModel(){
+	  }
+
+	  public void buildSchedule(){
+	  }
+
+	  public void buildDisplay(){
+	  }
+
+}
