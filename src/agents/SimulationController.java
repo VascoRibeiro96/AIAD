f9729284e0d314c7365537ea7074bb120024d426
Jonas L <joaohccl@live.com.pt>
2017-12-08 22:25:43 +0000

    commit antes de bazar

--- src/agents/SimulationController.java
+++ src/agents/SimulationController.java
@@ -14,10 +14,13 @@ public class SimulationController extends Agent{
 
     private final int totalParks;
     private final int totalDrivers;
+    private final int ticksPerSimulation;
+    private final int numberSimulations;
 
     class SimulationControllerBehaviour extends SimpleBehaviour{
 		private boolean end = false;
 		private int driversOut = 0;
+		private int curTick = 0;
 		private final Object lockInc = new Object();
 
         private SimulationControllerBehaviour(Agent a){
@@ -26,18 +29,54 @@ public class SimulationController extends Agent{
 
         @Override
         public void action() {
+            curTick++;
             ACLMessage msg = myAgent.receive();
+            if(curTick == ticksPerSimulation){ // should only happen once am i right?
+                informParkEnd();
+            }
             while (msg != null) {
                 if (msg.getPerformative() == ACLMessage.INFORM) {
                     if(msg.getContent().equals("driverOut")) {
-                        synchronized (lockInc) {
-                            driversOut++;
-                        }
+                        updateDriverCount();
                     }
+                    else rejectMessage(msg);
                 }
                 else rejectMessage(msg);
             }
             block();
+        }
+
+        private void updateDriverCount(){
+            synchronized (lockInc) {
+                driversOut++;
+                if(driversOut == totalDrivers){
+                    restartSimulation();
+                }
+            }
+        }
+
+        private void informParkEnd(){
+            // TODO enviar mensagem para os parques fecharem
+        }
+
+        // TODO mandar para os parques também
+        private void restartSimulation() {
+            // pesquisa DF por agentes "driver" para poder recomeçar a simulação
+            end = true;
+            DFAgentDescription template = new DFAgentDescription();
+            ServiceDescription sd1 = new ServiceDescription();
+            sd1.setType("Agente Driver");
+            template.addServices(sd1);
+            try {
+                DFAgentDescription[] result = DFService.search(myAgent, template);
+                ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+                for (DFAgentDescription aResult : result)
+                    msg.addReceiver(aResult.getName());
+                msg.setContent("Start");
+                send(msg);
+                System.out.println("Started Drivers!");
+            } catch(FIPAException e) { e.printStackTrace(); }
+            addBehaviour(new SimulationControllerBehaviour(myAgent));
         }
 
         private void rejectMessage(ACLMessage msg){
@@ -51,9 +90,11 @@ public class SimulationController extends Agent{
         public boolean done(){return end;}
     }
 
-    public SimulationController(int totalParks, int totalDrivers){
+    public SimulationController(int totalParks, int totalDrivers, int ticksPerSimulation, int numberSimulations){
         this.totalDrivers = totalDrivers;
         this.totalParks = totalParks;
+        this.ticksPerSimulation = ticksPerSimulation;
+        this.numberSimulations = numberSimulations;
     }
 
     protected void setup() {
@@ -73,22 +114,6 @@ public class SimulationController extends Agent{
         addBehaviour(new SimulationControllerBehaviour(this));
     }   // fim do metodo setup
 
-    private void restartSimulation() {
-        // pesquisa DF por agentes "driver" para poder recomeçar a simulação
-        DFAgentDescription template = new DFAgentDescription();
-        ServiceDescription sd1 = new ServiceDescription();
-        sd1.setType("Agente Driver");
-        template.addServices(sd1);
-        try {
-            DFAgentDescription[] result = DFService.search(this, template);
-            ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-            for (DFAgentDescription aResult : result)
-                msg.addReceiver(aResult.getName());
-            msg.setContent("Start");
-            send(msg);
-            System.out.println("Started Drivers!");
-        } catch(FIPAException e) { e.printStackTrace(); }
-    }
 
     protected void takeDown(){
         try {
